7251396b2c8c394ce3af9d1135a770877feba218
Rafael Costa <rafaelsevla@gmail.com>
2019-07-07 15:47:31 -0300

    refactoring app content and fetch user and populate state

--- src/app.js
+++ src/app.js
@@ -1,23 +1,40 @@
 'use strict';
 
 import React, { Component } from 'react';
+import ajax from '@fdaciuk/ajax';
 import AppContent from './components/app-content';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      userinfo: {
-        username: 'rafa',
-        photo: 'https://avatars3.githubusercontent.com/u/38443896?v=4',
-        login: 'rafaelsevla',
-        repos: 12,
-        followers: 10,
-        following: 20
-      },
-      repos: [{ name: 'repo', link: '#' }],
-      starred: [{ name: 'repo', link: '#' }]
+      userinfo: null,
+      repos: [],
+      starred: []
     };
+  }
+
+  handleSearch(e) {
+    const value = e.target.value;
+    const keyCode = e.which || e.keyCode;
+    const ENTER = 13;
+
+    if (keyCode === ENTER) {
+      ajax()
+        .get(`https://api.github.com/users/${value}`)
+        .then(result => {
+          this.setState({
+            userinfo: {
+              username: result.name,
+              photo: result.avatar_url,
+              login: result.login,
+              repos: result.public_repos,
+              followers: result.followers,
+              following: result.following
+            }
+          });
+        });
+    }
   }
 
   render() {
@@ -26,6 +43,7 @@ class App extends Component {
         userinfo={this.state.userinfo}
         repos={this.state.repos}
         starred={this.state.starred}
+        handleSearch={e => this.handleSearch(e)}
       />
     );
   }
